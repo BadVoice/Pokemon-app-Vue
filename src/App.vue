2691b6766c5a22bc3eb45d7b574c57ebcbeab8a9
Herman <smurovnemez@mail.ru>
2022-10-15 09:38:45 -0500

    feat: setting up app.vue

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,10 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        
-      </nav>
-    </div>
-  </header>
+  <div class="p-14 ">
+    <RouterLink class="flex justify-center text-4xl text-yellow-700"  to="/">Pokemon Picker</RouterLink>
+  </div>
 
   <RouterView />
 </template>
