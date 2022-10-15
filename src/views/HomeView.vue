c88e4120b4b2bcd210e61b4b0573bc996f6f56bf
Herman <smurovnemez@mail.ru>
2022-10-15 10:15:44 -0500

    feat: updating home.vue template/html

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,16 @@
 
 <template>
-  <div class="home">
-    <h3>Hello world</h3>
-    {{stateAsRefs.pokemons}}
+  <div class="w-full flex justify-center">
+    <input type="text" placeholder="Enter Pokemon here"
+    class="mt-10 p-2 border-vlue-500 border-2"
+    >
+  </div>
+
+  <div class="mt-10 p-4 flex flex-wrap justify-center">
+    <div class="ml-4 text-2x text-blue-500 "
+    v-for="(pokemon, idx) in state.pokemons" :key="idx">
+      {{pokemon.name}}
+    </div>
   </div>
 </template>
 
